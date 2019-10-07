7d218fabbe12b8acf5e376990570eadc168dfa1c
Corey Smaller <csmaller@macu.com>
2019-10-07 14:49:29 -0600

    first

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+import About from "./about.svelte";
+  export let name;
+</script>
+<!-- can we import scss like this?  sure can. -->
+<style src="./main.scss"></style>
+
+<h1>Hello {name}!</h1>
+
+<div>
+  <p>SASS is working!</p>
+  <About name={name} />
+</div>
---
+++ src/about.svelte
@@ -0,0 +1,8 @@
+<script>
+export let name;
+</script>
+<style src="./about.scss"></style>
+<div>
+    <p>hello! </p>
+    <h1>About us! {name} </h1>
+</div>
